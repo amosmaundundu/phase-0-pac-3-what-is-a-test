75b3c0d36cc9c70e8e036f7f5ac8a547b2dd9334
amosmaundundu <amosmaundu5@gmail.com>
2022-08-17 19:59:02 +0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+/*
+const { expect } = require("chai");
+
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
+
+console.log(message);
+*/
+
+
+
+describe("Name", ()=>{
+    it('returns "Susan"',() =>{ 
+        expect(Name).toEqual("Susan");
+    });
+});
+
+describe("Height", () =>{
+    it("is less than 40", ()=>{
+        expect (Height).toBeLessThan(40);
+    });
+});
+
+describe("message", ()=>{
+    it("gives the name and height", ()=>{
+        expect(message).toInclude(name);
+        expect(message).toInclude(height);
+    });
+
+    
+});
+
